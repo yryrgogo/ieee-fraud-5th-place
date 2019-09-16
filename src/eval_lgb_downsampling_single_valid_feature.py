01d143e623ef4bd906ec040c61d15524a0185167
yryrgogo <yryrgogo@gmail.com>
2019-09-16 09:00:59 +0000

    some

--- src/eval_lgb_downsampling_single_valid_feature.py
+++ src/eval_lgb_downsampling_single_valid_feature.py
@@ -124,9 +124,9 @@ fold_map = {
     2: '2018-3',
 }
 base_fold_score = {
-    0: 0.93550,
-    1: 0.94750,
-    2: 0.93750,
+    0: 0.93500,
+    1: 0.94700,
+    2: 0.93700,
 }
 
 print("Num Feature", len(valid_paths_train))
